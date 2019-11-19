7570ae8f30754d08ddc13d596f52d7eadc80d2eb
Bertil Chapuis <bchapuis@gmail.com>
2019-11-19 08:48:34 +0100

    fix packages

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.get('/image.png', function (req, res) {
     res.send(text2png(req.get('Cookie')));
 });
 app.post('/post', function (req, res) {
-    res.set('Access-Control-Allow-Origin', '*'); 
     res.json(req.get('Cookie'));
 });
 http.createServer(app).listen(3000, () => console.log("app app started"));
@@ -21,17 +20,21 @@ http.createServer(app).listen(3000, () => console.log("app app started"));
 // CSRF endpoint
 var csrf = express();
 csrf.get('/', function (req, res) {
+    console.log(req.get('Cookie'))
     res.send('<img src="http://localhost:3000/image.png" alt="csrf" />');
 });
 http.createServer(csrf).listen(3001, () => console.log("csrf endpoint started"));
 
 // https://blog.chromium.org/2019/10/developers-get-ready-for-new.html
 
+// The most common solution is to use CSRF tokens.
+// https://github.com/expressjs/csurf
+
 // SameSite is still experimental and requires to turn on features in the browser.
 // res.set('Set-Cookie', ['cookie=choco', 'SameSite=None', 'Secure']);
 
-// A solution is to use cors to prevent csrf with POST requests.
+// A partial solution to prevent CSRF with Fetch (or XMLHTTPRequest) is to configure CORS correctly.
 // res.set('Access-Control-Allow-Origin', 'http://localhost');
 
-// Another solution is to use CSRF token.
-// https://github.com/expressjs/csurf
+// Never do this except if you have a good reason!
+// Access-Control-Allow-Origin: *
